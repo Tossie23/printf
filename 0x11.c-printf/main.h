2dfb3c8fd929c81d25e33ddb8924f982127e81f6
Tossie23 <tossiechr@gmail.com>
2023-08-21 04:55:38 -0700

    0-printf.c

---
+++ 0x11.c-printf/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _strlen(char *s);
+void print_char_string_spec(char c, va_list args);
+
+#endif /* MAIN_H */
